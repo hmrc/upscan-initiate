526d4855c108486a716f0312504932d9ed246153
Nigel Perkins <nigel.perkins@digital.hmrc.gov.uk>
2020-02-27 15:27:15 +0000

    BDOG-643: uplift dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,8 +6,7 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-play-26" % "1.4.0",
     "com.typesafe.play" %% "play-json"         % "2.6.14",
-    "com.amazonaws"     % "aws-java-sdk-s3"    % "1.11.500",
-    "com.typesafe.akka" %% "akka-stream"       % "2.5.6"
+    "com.amazonaws"     % "aws-java-sdk-s3"    % "1.11.699"
   )
 
   trait TestDependencies {
@@ -16,19 +15,15 @@ object AppDependencies {
   }
 
   private def commonTestDependencies(scope: String) = Seq(
-    "uk.gov.hmrc"            %% "hmrctest"                    % "3.3.0"             % scope,
-    "uk.gov.hmrc"            %% "http-verbs-test"             % "1.3.0"             % scope,
+    "uk.gov.hmrc"            %% "hmrctest"                    % "3.9.0-play-26"     % scope,
     "com.typesafe.play"      %% "play-test"                   % PlayVersion.current % scope,
     "com.typesafe.play"      %% "play-ws"                     % PlayVersion.current % scope,
     "org.scalatest"          %% "scalatest"                   % "3.0.8"             % scope,
     "org.scalatestplus.play" %% "scalatestplus-play"          % "3.1.2"             % scope,
-    "org.pegdown"            % "pegdown"                      % "1.6.0"             % scope,
-    "org.mockito"            % "mockito-core"                 % "2.6.2"             % scope,
-    "com.github.tomakehurst" % "wiremock"                     % "2.2.2"             % scope,
-    "org.scalamock"          %% "scalamock-scalatest-support" % "3.5.0"             % scope,
-    "io.findify"             %% "s3mock"                      % "0.2.4"             % scope,
-    "commons-io"             % "commons-io"                   % "2.6"               % scope,
-    "org.scalacheck"         %% "scalacheck"                  % "1.13.4"            % scope
+    "org.pegdown"             % "pegdown"                     % "1.6.0"             % scope,
+    "org.mockito"             % "mockito-core"                % "3.3.0"             % scope,
+    "com.github.tomakehurst"  % "wiremock"                    % "2.26.0"            % scope,
+    "commons-io"              % "commons-io"                  % "2.6"               % scope
   )
 
   object Test {
