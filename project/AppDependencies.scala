4330243f6950006f35667f1e442dde3377f3f318
Nigel Perkins <nigel.perkins@digital.hmrc.gov.uk>
2020-03-02 11:30:44 +0000

    BDOG-643: changes in response to review comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,45 +3,21 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val compile = Seq(
+  private val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-play-26" % "1.4.0",
     "com.typesafe.play" %% "play-json"         % "2.6.14",
-    "com.amazonaws"     % "aws-java-sdk-s3"    % "1.11.699"
+    "com.amazonaws"      % "aws-java-sdk-s3"   % "1.11.699",
+    "org.apache.commons" % "commons-lang3"     % "3.9"
   )
 
-  trait TestDependencies {
-    lazy val scope: String       = "test"
-    lazy val test: Seq[ModuleID] = ???
-  }
-
-  private def commonTestDependencies(scope: String) = Seq(
-    "uk.gov.hmrc"            %% "hmrctest"                    % "3.9.0-play-26"     % scope,
-    "com.typesafe.play"      %% "play-test"                   % PlayVersion.current % scope,
-    "com.typesafe.play"      %% "play-ws"                     % PlayVersion.current % scope,
-    "org.scalatest"          %% "scalatest"                   % "3.0.8"             % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play"          % "3.1.2"             % scope,
-    "org.pegdown"             % "pegdown"                     % "1.6.0"             % scope,
-    "org.mockito"             % "mockito-core"                % "3.3.0"             % scope,
-    "com.github.tomakehurst"  % "wiremock"                    % "2.26.0"            % scope,
-    "commons-io"              % "commons-io"                  % "2.6"               % scope
+  private val test = Seq(
+    "uk.gov.hmrc"            %% "hmrctest"                    % "3.9.0-play-26"     % s"$Test,$IntegrationTest",
+    "com.typesafe.play"      %% "play-test"                   % PlayVersion.current % s"$Test,$IntegrationTest",
+    "org.scalatest"          %% "scalatest"                   % "3.0.8"             % s"$Test,$IntegrationTest",
+    "org.scalatestplus.play" %% "scalatestplus-play"          % "3.1.2"             % s"$Test,$IntegrationTest",
+    "org.pegdown"             % "pegdown"                     % "1.6.0"             % s"$Test,$IntegrationTest",
+    "org.mockito"             % "mockito-core"                % "3.3.0"             % s"$Test,$IntegrationTest"
   )
 
-  object Test {
-    def apply() =
-      new TestDependencies {
-        override lazy val test = commonTestDependencies(scope)
-      }.test
-  }
-
-  object IntegrationTest {
-    def apply() =
-      new TestDependencies {
-
-        override lazy val scope: String = "it"
-
-        override lazy val test = commonTestDependencies(scope)
-      }.test
-  }
-
-  def apply() = compile ++ Test() ++ IntegrationTest()
+  def apply(): Seq[ModuleID] = compile ++ test
 }
