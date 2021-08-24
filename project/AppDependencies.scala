b204cc3a1e378825cf22d3770e03a7c1d9b3b1fb
chotai <chotai@users.noreply.github.com>
2021-08-24 12:28:54 +0100

    BDOG-1471 Update bootstrap-play version to 28

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,21 +1,18 @@
 import sbt._
 
 object AppDependencies {
-  import play.core.PlayVersion
+  private val bootstrapVersion = "5.12.0"
 
   private val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-27"  % "3.2.0",
-    "com.typesafe.play" %% "play-json"                  % "2.7.4",
+    "uk.gov.hmrc"        %% "bootstrap-backend-play-28" % bootstrapVersion,
     "com.amazonaws"      % "aws-java-sdk-s3"            % "1.11.921",
     "org.apache.commons" % "commons-lang3"              % "3.11"
   )
 
   private val test = Seq(
-    "com.typesafe.play"      %% "play-test"                   % PlayVersion.current % s"$Test,$IntegrationTest",
-    "org.scalatest"          %% "scalatest"                   % "3.1.4"             % s"$Test,$IntegrationTest",
-    "org.scalatestplus.play" %% "scalatestplus-play"          % "4.0.3"             % s"$Test,$IntegrationTest",
-    "com.vladsch.flexmark"    % "flexmark-all"                % "0.35.10"           % s"$Test,$IntegrationTest",
-    "org.mockito"            %% "mockito-scala-scalatest"     % "1.15.1"            % Test
+    "uk.gov.hmrc"          %% "bootstrap-test-play-28"  % bootstrapVersion % s"$Test,$IntegrationTest",
+    "com.vladsch.flexmark" % "flexmark-all"             % "0.35.10"        % s"$Test,$IntegrationTest",
+    "org.mockito"          %% "mockito-scala-scalatest" % "1.16.25"        % Test
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
