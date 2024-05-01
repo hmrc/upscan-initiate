9763fa2d02e8dde1afe57d3554beb0d14c85acc4
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2024-05-01 10:22:54 +0100

    BDOG-3087 Update sbt-scoverage

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object AppDependencies {
-  private val bootstrapVersion = "8.4.0"
+  private val bootstrapVersion = "8.5.0"
 
   private val compile = Seq(
     "uk.gov.hmrc"        %% "bootstrap-backend-play-30" % bootstrapVersion,
