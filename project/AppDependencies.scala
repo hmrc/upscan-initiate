3635f4fc61f3f0d17ef3dadda1f13e9fcb2e9e74
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2026-05-20 12:03:46 +0100

    BDOG-3890: Update dependencies to address vulnerabilities

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object AppDependencies {
-  private val bootstrapVersion = "10.1.0"
-  private val awsSdkVersion = "2.35.1"
+  private val bootstrapVersion = "10.7.0"
+  private val awsSdkVersion = "2.44.7"
 
   private val compile = Seq(
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30" % bootstrapVersion,
